7549d3dfdf4aa2edf0e5e988c8a2b701374f8230
ejjays <christsonalloso021@gmail.com>
2026-08-03 00:12:54 +0800

    fix: drop coverage ci crash

--- packages/extractors/src/cli.ts
+++ packages/extractors/src/cli.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { spawn } from 'node:child_process';
+import { spawn } from 'node:child_process'; // skipcq: JS-0271
 import { createWriteStream } from 'node:fs';
 import { Readable, Writable } from 'node:stream';
 import { getExtractor } from './index.js';
